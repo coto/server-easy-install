8996357706d743f23c6db54ba548b39853109355
Rodrigo Augosto <rodrigo.augosto@gmail.com>
2010-05-11 17:54:59 -0400

    file to update sandbox

---
+++ utilities/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Updating Sandbox..."
+sandbox_dir='/home/coto/sandbox'
+dir=`ls $sandbox_dir`
+for d in $dir
+do	
+	if [ -d $sandbox_dir/$d/.svn ];
+	then
+		cd $sandbox_dir/$d
+		echo -e "\033[4m$d (svn)\e[0m"
+		svn update
+	elif [ -d $sandbox_dir/$d/.git ];
+	then
+		cd $sandbox_dir/$d
+		echo -e "\033[4m$d (git)\e[0m"
+		git pull origin master 
+	fi
+done
+echo "Sandbox Updated Successfully!"
